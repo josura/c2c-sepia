bcb288e694d09b99edf28ba8e4e010757365f073
josura <giorgiolocicero@hotmail.com>
2023-01-27 17:50:21 +0100

    moved implementation in the header since they are single lines of code

--- src/armaUtilities.h
+++ src/armaUtilities.h
@@ -2,10 +2,10 @@
 #include <vector>
 
 template<typename T>
-std::vector<std::vector<T>> armaMatrixToVector(arma::Mat<T> matr);
+std::vector<std::vector<T>> armaMatrixToVector(arma::Mat<T> matr){return arma::conv_to< std::vector<T> >::from(arma::vectorise( matr));}
 
 template<typename T>
-std::vector<T> armaColumnToVector(arma::Col<T> matr);
+std::vector<T> armaColumnToVector(arma::Col<T> matr){return arma::conv_to< std::vector<T> >::from(matr);}
 
 template<typename T>
-std::vector<T> armaRowToVector(arma::Row<T> matr);
+std::vector<T> armaRowToVector(arma::Row<T> matr){return arma::conv_to< std::vector<T> >::from(matr);}
