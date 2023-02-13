ea1ec1b72a10d038756f7c1e6c576fdde34afbb4
josura <giorgiolocicero@hotmail.com>
2023-02-13 15:30:17 +0100

    additional function to add edges

--- src/Computation.h
+++ src/Computation.h
@@ -45,6 +45,9 @@ class Computation{
         */
         void augmentMetapathway(const std::vector<std::string>&,const std::vector<std::pair<std::string,std::string>>& newEdgesList =std::vector<std::pair<std::string,std::string>>(), const std::vector<double>& newEdgesValue = std::vector<double>(), bool includeSelfVirtual=false);
         void addEdges(const std::vector<std::pair<std::string,std::string>>& , const std::vector<double>& , bool bothDirections = false);
+        void addEdges(const std::vector<std::tuple<std::string,std::string,double>>&  , bool bothDirections = false);
+        void addEdges(const std::vector<std::pair<int,int>>& , const std::vector<double>& , bool bothDirections = false);
+        void addEdges(const std::vector<std::tuple<int,int,double>>&  , bool bothDirections = false);
         std::vector<double> computePerturbation();
         std::vector<double> computeAugmentedPerturbation(); //taking into account virtual nodes in the augmented metapathway
         void updateInput(const std::vector<double>& newInp = std::vector<double>(), bool augmented = false);
