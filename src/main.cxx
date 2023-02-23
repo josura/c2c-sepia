3b1138058df2794c0e43e74af0fc92fe46beba93
josura <giorgiolocicero@hotmail.com>
2023-02-23 22:03:43 +0100

    init parsing from boost library

--- src/main.cxx
+++ src/main.cxx
@@ -1,4 +1,6 @@
+#include <boost/program_options/value_semantic.hpp>
 #include <iostream>
+#include <boost/program_options.hpp>
 #include "Computation.h"
 #include "WeightedEdgeGraph.h"
 #include "utilities.h"
@@ -9,6 +11,34 @@ void printHelp(){
 }
 
 int main(int argc, char** argv ) {
+    //program options
+    bool ensembleGeneNames=false;
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "print help section <logfoldPerCell>.tsv [<subcelltypes>.txt] [<celltypesInteraction>.tsv]\nFILE STRUCTURE SCHEMA:\nmetapathway.tsv\nstart\tend\tweight\n<gene1>\t<gene2>\t <0.something>\n...\n\n\nlogfoldPerCell.tsv\n\tcell1\tcell2\t...\tcellN\ngene1\t<lfc_cell1:gene1>\t<lfc_cell2:gene1>\t...\t<lfc_cellN:gene1>\ngene1\t<lfc_cell1:gene2>\t<lfc_cell2:gene2>\t...\t<lfc_cellN:gene2>\n...\n\n\ncelltypesInteraction.tsv\nstartCell:geneLigand\tendCell:geneReceptor\tweight\n<cell1:geneLigand>\t<cell2:genereceptor>\t <0.something>\n...\n\n\nsubcelltypes.txt\ncell1\ncell3\n...")
+        ("fMETAPATHWAY", po::value<std::string>(), "metapathway filename, for an example metapathway see in resources")
+        ("fLogfoldPerCell", po::value<std::string>(), "logfoldPerCell matrix filename, for an example see in data")
+        ("ensembleGeneNames",po::bool_switch(&ensembleGeneNames),"use ensemble gene names, since the metapathway used in resources have entrez_ids, a map will be done from ensemble to entrez, the map is available in resources")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        //printHelp();
+        return 1;
+    }
+
+    if (vm.count("-fMETAPATHWAY")) {
+        std::cout << "file for the metapathway was set to " 
+    << vm["fMETAPATHWAY"].as<std::string>() << ".\n";
+    } else {
+        std::cout << "fMETAPATHWAY file was not set. Aborting\n";
+        return 1;
+    }
+    //end program options section
     WeightedEdgeGraph* g1 = new WeightedEdgeGraph(4);
     WeightedEdgeGraph* g2 = new WeightedEdgeGraph();
     std::cout << " testing "<< g2->getNumEdges()<<std::endl;
