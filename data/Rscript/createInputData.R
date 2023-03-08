f995d7878d0b9fd1535c1c6b9ac01ccf4dbd7fef
josura <giorgiolocicero@hotmail.com>
2023-03-08 22:58:26 +0100

    fixed sepia input creation for log fold

--- data/Rscript/createInputData.R
+++ data/Rscript/createInputData.R
@@ -13,24 +13,20 @@ dim(table(tisch.logFoldChange$Gene))
 library(dplyr)
 
 table(tisch.logFoldChange$Celltype..minor.lineage.)
-
 tisch.dataframe <- tisch.logFoldChange
 cell.list <- unique(tisch.dataframe$Celltype..minor.lineage.)
 cell.logfold.list <- list()
-for (cell.name in cell.list) {
-  sub.tisch.dataframe <- tisch.dataframe %>%
-    filter(Celltype..minor.lineage. == cell.name)
-  logfold.embed <- data.frame(t(unlist(sub.tisch.dataframe$log2FC)),row.names = cell.name)
-  colnames(logfold.embed) <- sub.tisch.dataframe$Gene
-  #logfold.embed$gene.name <- cell.name
-  cell.logfold.list[[length(cell.logfold.list)+1]] <- logfold.embed
-  print(paste("logFolds done for cell",cell.name,sep = ": "))
-}
-merged.embeddings <- cell.logfold.list[[1]]
-cell.logfold.list <- cell.logfold.list[-1]
 
-merged.embeddings <- data.table::rbindlist(list(merged.embeddings,cell.logfold.list[1]),fill= TRUE)
-merged.embeddings <- bind_rows(merged.embeddings,cell.logfold.list[1])
+cell.name <- cell.list[2]
+sub.tisch.dataframe <- tisch.dataframe %>%
+  filter(Celltype..minor.lineage. == cell.name)
+
+
+gene.names.noversion <- lapply(sub.tisch.dataframe$Gene,  sub, pattern = "\\.\\d+$", replacement = "")
+rownames(sub.tisch.dataframe) <- gene.names.noversion
+logfold.embed <- data.frame(t(unlist(sub.tisch.dataframe$log2FC)),row.names = cell.name)
+colnames(logfold.embed) <- gene.names.noversion
+
 
 create.logfold.input <- function(tisch.dataframe){
   #TODO control input
@@ -39,8 +35,18 @@ create.logfold.input <- function(tisch.dataframe){
   for (cell.name in cell.list) {
     sub.tisch.dataframe <- tisch.dataframe %>%
       filter(Celltype..minor.lineage. == cell.name)
-    logfold.embed <- data.frame(t(unlist(sub.tisch.dataframe$log2FC)),row.names = cell.name)
-    colnames(logfold.embed) <- sub.tisch.dataframe$Gene
+    #gene.names.noversion <- lapply(sub.tisch.dataframe$Gene,  gsub, pattern = "\"", replacement = "")
+    gene.names.noversion <- lapply(sub.tisch.dataframe$Gene,  sub, pattern = "\\.\\d+$", replacement = "")
+    sub.tisch.dataframe$Genefiltered <- gene.names.noversion
+    sub.tisch.dataframe.filtered <- sub.tisch.dataframe %>%
+      group_by(Genefiltered) %>%
+      #arrange(log2FC) %>%
+      slice(1) %>% ungroup
+    
+    gene.names.noversion <- sub.tisch.dataframe.filtered$Genefiltered
+    logfold.embed <- data.frame(t(unlist(sub.tisch.dataframe.filtered$log2FC)),row.names = cell.name)
+    
+    colnames(logfold.embed) <- gene.names.noversion
     #logfold.embed$gene.name <- cell.name
     cell.logfold.list[[length(cell.logfold.list)+1]] <- logfold.embed
     print(paste("logFolds done for cell",cell.name,sep = ": "))
@@ -48,7 +54,10 @@ create.logfold.input <- function(tisch.dataframe){
   merged.embeddings <- cell.logfold.list[[1]]
   cell.logfold.list <- cell.logfold.list[-1]
   for (path.embed in cell.logfold.list) {
-    merged.embeddings <- bind_rows(merged.embeddings,path.embed)
+    #merged.embeddings <- bind_rows(merged.embeddings,path.embed)
+    merged.embeddings[setdiff(names(path.embed), names(merged.embeddings))] <- NA
+    path.embed[setdiff(names(merged.embeddings), names(path.embed))] <- NA
+    merged.embeddings <- bind_rows(merged.embeddings, path.embed)
     print(paste("merge done for cell",rownames(path.embed),sep = ": "))
   }
   merged.embeddings[is.na(merged.embeddings)] <- 0
@@ -56,7 +65,7 @@ create.logfold.input <- function(tisch.dataframe){
 }
 
 test <- create.logfold.input(tisch.logFoldChange)
-t(test)
+write.table(t(test),"logfoldchangeMatrix/tisch_ESCA_GSE160269-logfold-noversion.tsv",sep = "\t",quote = FALSE)
 
 devtools::install_github("sqjin/CellChat")
 
