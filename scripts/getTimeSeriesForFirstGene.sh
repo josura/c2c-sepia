5028b06ced25c524a8db0b708c1adaf068a9c95e
josura <giorgiolocicero@hotmail.com>
2023-03-27 10:07:41 +0200

    Ok I have done it for the first gene, to get all genes, I will use R since bash is trash for these kind of things

--- scripts/getTimeSeriesForFirstGene.sh
+++ scripts/getTimeSeriesForFirstGene.sh
@@ -16,15 +16,19 @@ for prefix in $(ls $input_dir | cut -d '-' -f 1 | sort -u); do
 
   # loop over each file with the current prefix
   for file in $(ls $input_dir | grep "^$prefix"); do
+    echo "getting first gene value for file $file"
 
     # get the feature name from the file name
-    feature=$(echo $file | cut -d '-' -f 2 | cut -d '.' -f 1)
+    feature=$(echo $file | cut -d '-' -f 3 | cut -d '.' -f 1)
 
     # get the fifth value from the current file
     value=$(awk 'NR==5 {print $0}' $input_dir/$file)
 
+    echo -n $file | cut -d '-' -f 3 | cut -d '.' -f 1 | tr -d $'\n' >> $output_dir/${prefix}_output.tsv
+
     # append the feature and value to the output file
-    echo -e "$feature\t$value" >> $output_dir/${prefix}_output.tsv
+    cat $input_dir/$file | awk '{if(NR==2) printf("\t%s\n",$5)}'  >> $output_dir/${prefix}_output.tsv
+    #echo -e "$feature\t$value" >> $output_dir/${prefix}_output.tsv
 
   done
 
