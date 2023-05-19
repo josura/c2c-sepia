d30901fd6e0568c4c36933a0974935b3ad7b1ac9
josura <giorgiolocicero@hotmail.com>
2023-05-19 16:12:26 +0200

    dissipate pow 2 self

--- src/DissipationModel.cxx
+++ src/DissipationModel.cxx
@@ -1,1 +1,12 @@
 #include "DissipationModel.h"
+
+DissipationModel::DissipationModel(){
+}
+
+DissipationModel::~DissipationModel(){
+}
+
+arma::Col<double> DissipationModel::dissipatePow2Self(arma::Col<double> input){
+    return input - pow(input,2);
+}
+
