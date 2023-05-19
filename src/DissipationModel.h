72802d619ac3ff071f1e0d105fea372b8a7a4eb8
josura <giorgiolocicero@hotmail.com>
2023-05-19 15:26:16 +0200

    dissipation models declarations

--- src/DissipationModel.h
+++ src/DissipationModel.h
@@ -6,4 +6,11 @@ class DissipationModel{
     ~DissipationModel();
     public:
         arma::Col<double> dissipatePow2Self(arma::Col<double> input);
+        arma::Col<double> dissipateSelfScaled(arma::Col<double> input, double scale);
+        arma::Col<double> dissipateSelfScaled(arma::Col<double> input, arma::Col<double> scales);
+        arma::Col<double> dissipateSelfPeriodic(arma::Col<double> input, double period, double amplitude, double phase);
+        arma::Col<double> dissipateSelfPeriodic(arma::Col<double> input, arma::Col<double> periods, arma::Col<double> amplitudes, arma::Col<double> phases);
+        arma::Col<double> dissipateSelfPeriodicShift(arma::Col<double> input, double shiftY, double period, double amplitude, double phase);
+        arma::Col<double> dissipateSelfPeriodicShift(arma::Col<double> input, arma::Col<double> shiftY, arma::Col<double> periods, arma::Col<double> amplitudes, arma::Col<double> phases);
+        arma::Col<double> dissipateSelfRandom(arma::Col<double> input, double min, double max);
 };
