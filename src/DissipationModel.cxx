c9428b479bcd899ab223f19826cb45656a4e2e85
josura <giorgiolocicero@hotmail.com>
2023-05-19 16:16:57 +0200

    dissipate self scaled

--- src/DissipationModel.cxx
+++ src/DissipationModel.cxx
@@ -10,3 +10,10 @@ arma::Col<double> DissipationModel::dissipatePow2Self(arma::Col<double> input){
     return input - pow(input,2);
 }
 
+arma::Col<double> DissipationModel::dissipateSelfScaled(arma::Col<double> input, double scale){
+    return input - scale*input;
+}
+
+arma::Col<double> DissipationModel::dissipateSelfScaled(arma::Col<double> input, arma::Col<double> scales){
+    return input - scales%input;
+}
