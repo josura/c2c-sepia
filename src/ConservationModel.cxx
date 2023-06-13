9567a424d8dcff5632aac6ebacd14845ca67f6cb
josura <giorgiolocicero@hotmail.com>
2023-06-13 16:58:17 +0200

    Added conservation model (to test) to address #17

---
+++ src/ConservationModel.cxx
@@ -0,0 +1,52 @@
+#include "ConservationModel.h"
+
+
+ConservationModel::ConservationModel(){
+    this->scaleFunction = [](double time)-> double{return 0.5;};
+}
+
+ConservationModel::ConservationModel(std::function<double(double)> scaleFunction){
+    this->scaleFunction = scaleFunction;
+}
+
+ConservationModel::~ConservationModel(){}
+
+arma::Col<double> ConservationModel::conservate(arma::Col<double> input, arma::Col<double> inputDissipated, arma::Mat<double> Wstar,double time, std::vector<double> q){
+    //if q is empty, then we assume that all the values in q are 1, that means all the weights of the edges are considered of the same importance and 
+    // and the perturbation is completely passed down the network 
+    if (q.size()) {
+        if (q.size() == input.n_elem) {
+            //convert q vector to arma vector
+            arma::Col<double> qArma = vectorToArmaColumn(q);
+            arma::Col<double> outputArma = inputDissipated -  scaleFunction(time) * Wstar * qArma * input;
+            return outputArma;
+        } else{
+            throw std::invalid_argument("q vector is not of the same size as input vector. abort");
+        }
+    }
+    else {
+        //since in the case of vector values with all q values equal to 1
+        arma::Col<double> qOnes = arma::ones<arma::Col<double>>(input.n_elem);
+        arma::Col<double> outputArma =  inputDissipated -  scaleFunction(time)* Wstar * qOnes * input;
+        return outputArma;
+    }
+}
+
+arma::Col<double> ConservationModel::conservationTerm(arma::Col<double> input, arma::Mat<double> Wstar, double time, std::vector<double> q){
+    if (q.size()) {
+        if (q.size() == input.n_elem) {
+            //convert q vector to arma vector
+            arma::Col<double> qArma = vectorToArmaColumn(q);
+            arma::Col<double> outputArma =  scaleFunction(time) * Wstar * qArma * input;
+            return outputArma;
+        } else{
+            throw std::invalid_argument("q is not of the same size as input vector. abort");
+        }
+    } else{
+        arma::Col<double> qOnes = arma::ones<arma::Col<double>>(input.n_elem);
+        arma::Col<double> outputArma =  scaleFunction(time) * Wstar * qOnes * input;
+        return outputArma;
+    }
+}
+
+
---
+++ src/ConservationModel.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <armadillo>
+#include <functional>
+#include "armaUtilities.h"
+
+class ConservationModel{
+    protected:
+        std::function<double(double)> scaleFunction;
+    public:
+        ConservationModel();
+        ConservationModel(std::function<double(double)> scaleFunction);
+        virtual ~ConservationModel();
+        //using the scale function as a parameter itself, dependency injection
+        virtual arma::Col<double> conservate(arma::Col<double> input, arma::Col<double> inputDissipated,arma::Mat<double> Wstar, double time, std::vector<double> q = std::vector<double>());
+        virtual arma::Col<double> conservationTerm(arma::Col<double> input,arma::Mat<double> Wstar, double time, std::vector<double> q = std::vector<double>());
+};
