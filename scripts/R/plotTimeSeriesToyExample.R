8543885b895ac4736ae8f31409647ecb33f1eb25
josura <giorgiolocicero@hotmail.com>
2023-07-18 13:39:46 +0200

    fixed modified files for the graph

--- scripts/R/plotTimeSeriesToyExample.R
+++ scripts/R/plotTimeSeriesToyExample.R
@@ -13,31 +13,6 @@ y <- timeSeries.t2[, (ncol(timeSeries.t2)-11):ncol(timeSeries.t2)]
 interesting.list <- c("v.in.t3","v.in.t1","v.out.t3","v.out.t1","v.out.t2")
 y.int <- timeSeries.t2[, interesting.list]
 
-# Combine x and y into a new dataframe
-df_new <- data.frame(x = rep(x, ncol(y)), y)
-
-# Convert y columns into a single column
-df_new <- df_new %>%
-  pivot_longer(cols = starts_with("v."), names_to = "variable", values_to = "value")
-
-# Plot the matrix of scatter plots
-ggplot(df_new, aes(x = x, y = value, color = variable)) +
-  geom_point() +
-  labs(x = "X Axis", y = "Y Axis") +
-  scale_color_discrete(name = "Y Variables") +
-  facet_wrap(~ variable, ncol = 3)  # Adjust the number of columns as needed
-
-
-# Combine x and y into a new dataframe
-df_new <- data.frame(x = rep(x, ncol(y)))
-df_new <- cbind(df_new, y)
-
-# Reshape the data by converting columns into a single column
-df_new <- data.frame(
-  x = rep(df_new$x, times = ncol(y)),
-  variable = rep(colnames(y), each = nrow(y)),
-  value = c(t(y))
-)
 
 
 # my reshaping since everything else doesn't work
@@ -60,5 +35,5 @@ ggplot(df_new_corrected, aes(x = x, y = value, color = variable)) +
   facet_wrap(~ variable, ncol = 3)  # Adjust the number of columns as needed
 
 
-plot(df_new[df_new$variable == "v.in.t1",]$x,df_new[df_new$variable == "v.in.t1",]$value)
+plot(df_new_corrected[df_new_corrected$variable == "a",]$x,df_new_corrected[df_new_corrected$variable == "a",]$value)
 plot( timeSeries.t2$iteration, timeSeries.t2$v.in.t1)
