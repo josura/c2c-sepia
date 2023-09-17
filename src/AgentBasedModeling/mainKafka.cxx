07f0b0aba941eb4e9fe5ed8cd837d72d1a191334
josura <giorgiolocicero@hotmail.com>
2023-09-18 00:28:01 +0200

    init code for MPI and Kafka

---
+++ src/AgentBasedModeling/mainKafka.cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// #include "AgentBasedModeling/Agent.h"
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello, world!" << std::endl;
+
+//   std::vector<Agent> agents;
+//   agents.push_back(Agent(1, 2, 3));
+//   agents.push_back(Agent(4, 5, 6));
+//   agents.push_back(Agent(7, 8, 9));
+
+//   for (auto& agent : agents) {
+//     std::cout << agent << std::endl;
+//   }
+
+  return 0;
+}
---
+++ src/AgentBasedModeling/mainMPI.cxx
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int numProcesses, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Assuming types is the workload to be distributed
+    std::vector<std::string> types = {"type1", "type2", "type3", "type4"};
+
+    int workloadPerProcess = types.size() / numProcesses;
+    int startIdx = rank * workloadPerProcess;
+    int endIdx = (rank == numProcesses - 1) ? types.size() : (rank + 1) * workloadPerProcess;
+
+    // Each process works on its assigned workload
+    for (int i = startIdx; i < endIdx; ++i) {
+        // Compute for types[i]
+        std::cout << "Process " << rank << " is computing for type: " << types[i] << "\n";
+        // ... Perform computation for types[i]
+    }
+
+    MPI_Finalize();
+    return 0;
+}
