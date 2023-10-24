76f7c39f1a9ad555d456bf1ba214282a3fe41929
josura <giorgiolocicero@hotmail.com>
2023-10-24 22:04:04 +0200

    removed some logging

--- src/DissipationModelScaled.cxx
+++ src/DissipationModelScaled.cxx
@@ -12,15 +12,12 @@ DissipationModelScaled::~DissipationModelScaled(){
 DissipationModelScaled::DissipationModelScaled(std::function<double(double)> scaleFun){
     this->scaleFunction = scaleFun;
     this->numEl = 0;
-    std::cout << "DissipationModelScaled constructor" << std::endl;
 }
 
 arma::Col<double> DissipationModelScaled::dissipate(arma::Col<double> input, double time){
     return input - (this->scaleFunction(time)*input);
-    std::cout << "dissipate from scaled" << std::endl;
 }
 
 arma::Col<double> DissipationModelScaled::dissipationTerm(arma::Col<double> input, double time){
     return this->scaleFunction(time)*input;
-    std::cout << "dissipate term from scaled" << std::endl;
 }
