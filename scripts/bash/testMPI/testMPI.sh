32edb861aec1290eb17939063723a7d663a43859
josura <giorgiolocicero@hotmail.com>
2023-11-20 21:57:45 +0100

    changed script to test MPI

--- scripts/bash/testMPI/testMPI.sh
+++ scripts/bash/testMPI/testMPI.sh
@@ -1,4 +1,7 @@
-mpirun -np ./build/c2c-sepia --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+# interface is passed as an argument
+interface=$1
+
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include $interface -np 2 ./build/c2c-sepia-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
             --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousGraph/initialValuesPartialTypes \
             --typeInteractionFolder data/testdata/testHeterogeneousGraph/interactions \
             --dissipationModel scaled \
