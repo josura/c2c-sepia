08ba84f5f5c5794945f1993f03f59636b268382e
josura <giorgiolocicero@hotmail.com>
2023-11-21 14:10:09 +0100

    script to test if MPI implementation leads to the same results as normal implementation

---
+++ scripts/bash/testMPI/test-example.sh
@@ -0,0 +1,11 @@
+./build/c2c-sepia --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+            --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousGraph/initialValuesPartialTypes \
+            --typeInteractionFolder data/testdata/testHeterogeneousGraph/interactions \
+            --dissipationModel scaled \
+            --dissipationModelParameters 0.2 \
+            --propagationModel neighbors \
+            --propagationModelParameters 0.2 \
+            --saturation \
+            --undirectedEdges \
+            --undirectedTypeEdges \
+            --outputFolder outputs/testing-example
