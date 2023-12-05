2779d2031ef72cb29899191eba46b06b3e034d4f
josura <giorgiolocicero@hotmail.com>
2023-12-05 13:31:58 +0100

    forgot to close the MPI issue with the solution, the problem was that the communication between different processes was using more than one interface to communicate and some of them were probably not ready or were firewalled, specifiyng an interface has resolved the issue, (close #37)

--- scripts/bash/testMPI/testMPI.sh
+++ scripts/bash/testMPI/testMPI.sh
@@ -1,14 +1,14 @@
 # interface is passed as an argument
 interface=$1
 
-mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include $interface -np 2 ./build/c2c-sepia-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
+mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include $interface -np 4 ./build/c2c-sepia-MPI --graphsFilesFolder data/testdata/testHeterogeneousGraph/graphs \
             --initialPerturbationPerTypeFolder data/testdata/testHeterogeneousGraph/initialValuesPartialTypes \
             --typeInteractionFolder data/testdata/testHeterogeneousGraph/interactions \
             --dissipationModel scaled \
             --dissipationModelParameters 0.2 \
             --conservationModel scaled \
             --conservationModelParameters 0.2 \
-            --propagationModel neighbors \
+            --propagationModel customPropagation \
             --propagationModelParameters 0.2 \
             --saturation \
             --undirectedEdges \
