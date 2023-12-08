3152430e7b32fd7ea0307f2e65385d8f25c43188
josura <giorgiolocicero@hotmail.com>
2023-12-08 10:23:40 +0100

    fixed shell script to do the performance experiments

--- scripts/performanceTesting/generatePerformanceTimesDifferentGraphs.sh
+++ scripts/performanceTesting/generatePerformanceTimesDifferentGraphs.sh
@@ -2,6 +2,7 @@
 
 # control if parameters are passed
 if [ $# -ne 3 ]; then
+    echo $# arguments passed
     echo "Please pass the interface, the folder with the inputs and the folder where to save the outputs"
     exit 1
 fi
@@ -13,28 +14,27 @@ interface=$1
 inputsFolder=$2
 outputsFolder=$3
 
-#get the graph file, the initial perturbation and the type interactions from the folder
-graphsFolderName=$(ls $inputsFolder | grep "graph")
-initialPerturbationFolderName=$(ls $inputsFolder | grep "node_conditions_discr")
-typeInteractionsFolderName=$(ls $inputsFolder | grep "interactions")
-
-# get the full path for the graph file, the initial perturbation and the type interactions 
-graphsFolder=$inputsFolder$graphsFolderName
-initialPerturbationFolder=$inputsFolder$initialPerturbationFolderName
-typeInteractionsFolder=$inputsFolder$typeInteractionsFolderName
-
-echo "Graph file: $graphsFolderName"
-echo "Initial perturbation file: $initialPerturbationFolderName"
-echo "Type interactions file: $typeInteractionsFolderName"
-
-
 
 # loop through the files (the name of the file should be <numnodes>Nodes.tsv), the number of nodes start from 1000 and increase by 1000 until 30000
-for graphFile in $(ls $graphsFolder | grep "Nodes.tsv"); do
-    echo "Graph file: $graphFile"
+for graphFolder in $(ls $inputsFolder | grep "Nodes"); do
+    echo "Graph folder: $inputsFolder$graphFolder"
+
+    #get the graph file, the initial perturbation and the type interactions from the folder
+    graphsFolderName=$(ls $inputsFolder$graphFolder | grep "graph")
+    initialPerturbationFolderName=$(ls $inputsFolder$graphFolder | grep "node_conditions_discr")
+    typeInteractionsFolderName=$(ls $inputsFolder$graphFolder | grep "interactions")
+
+    # get the full path for the graph file, the initial perturbation and the type interactions 
+    graphsFolder=$inputsFolder$graphFolder$graphsFolderName
+    initialPerturbationFolder=$inputsFolder$graphFolder$initialPerturbationFolderName
+    typeInteractionsFolder=$inputsFolder$graphFolder$typeInteractionsFolderName
+
+    echo "Graph file: $graphsFolderName"
+    echo "Initial perturbation file: $initialPerturbationFolderName"
+    echo "Type interactions file: $typeInteractionsFolderName"
+
     # get the number of nodes from the file name
-    numNodes=$(echo $graphFile | cut -d'N' -f 1)
-    echo "$numNodes\t" >> $outputsFolder/times.txt
+    numNodes=$(echo $graphsFolderName | cut -d'N' -f 1)
     # run the simulation
     echo "mpirun --mca pml ob1 --mca btl tcp,self --mca btl_tcp_if_include $interface -np 4 ./build/c2c-sepia-MPI \ 
                 --graphsFilesFolder $graphsFolder \
