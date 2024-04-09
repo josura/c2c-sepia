8e5ed5b5ed3bfca804f4195d1ad06392f3e32f82
josura <giorgiolocicero@hotmail.com>
2024-04-09 11:18:21 +0200

    continuing with the implementation of the code in python to simulate the SIS model to compare the results of MASFENON

--- scripts/python/BMC/EoFexperiments.py
+++ scripts/python/BMC/EoFexperiments.py
@@ -2,3 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import EoN
 
+# get the network from one of the generated files that contains the edge data (source, target, weight)
+graphEdgesFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmentAging/10000nodes/1/edge_data.tsv'
+G = nx.read_edgelist(graphEdgesFile, delimiter='\t', data=(('weight', float),))
+
+# set the parameters for the simulation
+gamma = 1
+tau = 0.2
+rho = 0.005
+# read the initial condition of every node from a file
+initialConditionFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmentAging/10000nodes/1/node_condititions.tsv'
