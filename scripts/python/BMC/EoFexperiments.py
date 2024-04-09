81d9f28542f8c969964f3b8c46f38ea68e4e8719
josura <giorgiolocicero@hotmail.com>
2024-04-09 12:23:27 +0200

    python code continuation with the parameters used for the simulation

--- scripts/python/BMC/EoFexperiments.py
+++ scripts/python/BMC/EoFexperiments.py
@@ -7,8 +7,7 @@ graphEdgesFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmen
 G = nx.read_edgelist(graphEdgesFile, delimiter='\t', data=(('weight', float),))
 
 # set the parameters for the simulation
-gamma = 1
-tau = 0.2
-rho = 0.005
+gamma = 0.2
+tau = 0.5
 # read the initial condition of every node from a file
 initialConditionFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmentAging/10000nodes/1/node_condititions.tsv'
