d5ae14f5e0ff1207384f39f50638cf73b5121484
josura <giorgiolocicero@hotmail.com>
2024-04-27 22:47:43 +0200

    starting to write the code to run the simulation of the EoN package

--- scripts/python/BMC/EoFexperiments.py
+++ scripts/python/BMC/EoFexperiments.py
@@ -3,11 +3,28 @@ import matplotlib.pyplot as plt
 import EoN
 
 # get the network from one of the generated files that contains the edge data (source, target, weight)
-graphEdgesFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmentAging/10000nodes/1/edge_data.tsv'
+graphEdgesFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachment/10000nodes/1/edge_data.tsv'
 G = nx.read_edgelist(graphEdgesFile, delimiter='\t', data=(('weight', float),))
 
 # set the parameters for the simulation
 gamma = 0.2
 tau = 0.5
 # read the initial condition of every node from a file
-initialConditionFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachmentAging/10000nodes/1/node_condititions.tsv'
+# the file should have the following format:
+# node_name  condition(Susceptible, Infected)
+initialConditionFile = 'scripts/R/epidemics/significanceAnalysys/preferentialAttachment/10000nodes/1/node_condititions.tsv'
+initialConditions = {}
+with open(initialConditionFile, 'r') as f:
+    for line in f:
+        node, condition = line.strip().split('\t')
+        initialConditions[node] = condition
+
+# get the initial infecteds
+initial_infecteds = []
+for node in G.nodes():
+    if initialConditions[node] == 'Infected':
+        initial_infecteds.append(node)
+
+
+# run the simulation in the graphs
+t, S, I, R = EoN.fast_SIR(G, tau, gamma, initial_infecteds, tmax=float('inf'), return_full_data=True)
