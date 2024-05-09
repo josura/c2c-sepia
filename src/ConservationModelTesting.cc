aeae08748a738b04a955f317b12dd1fe725985f8
josura <giorgiolocicero@hotmail.com>
2024-05-09 12:54:09 +0200

    testing conservation model

--- src/ConservationModelTesting.cc
+++ src/ConservationModelTesting.cc
@@ -10,6 +10,12 @@ class ConservationModelTesting : public ::testing::Test {
         void SetUp() override {
             c0 = new ConservationModel();
             c1 = new ConservationModel([](double time)->double{return 0;});
+
+            Wstar_oneEdge(0,1) = 1;
+
+            Wstar_threeEdges(0,1) = 1;
+            Wstar_threeEdges(1,2) = 1;
+            Wstar_threeEdges(2,0) = 1;
         }
         void TearDown() override{
             delete c0;
@@ -17,23 +23,48 @@ class ConservationModelTesting : public ::testing::Test {
         }
         ConservationModel* c0;       //testing default constructor
         ConservationModel* c1;       //testing general constructor
+        arma::Col<double> input = {1,2,3};
+        arma::Col<double> inputDissipated = {1,2,3};
+        arma::dmat Wstar_zeros = arma::zeros<arma::dmat>(3,3);
+        arma::dmat Wstar_oneEdge = arma::zeros<arma::dmat>(3,3);
+        arma::dmat Wstar_threeEdges = arma::zeros<arma::dmat>(3,3);
+        std::vector<double> q = {1,1,1};
   
 };
 
 TEST_F(ConservationModelTesting, constructorWorksDefault) {
-    EXPECT_EQ(c0->getScaleFunction()(0),0.5);
+    EXPECT_DOUBLE_EQ(c0->getScaleFunction()(0),0.5);
 }
 
 TEST_F(ConservationModelTesting, constructorWorksGeneral) {
-    EXPECT_EQ(c1->getScaleFunction()(0),0);
+    EXPECT_DOUBLE_EQ(c1->getScaleFunction()(0),0);
 }
 
-// TEST_F(ConservationModelTesting, conservateWorks) {
-//     arma::Col<double> input = {1,2,3};
-//     arma::Col<double> inputDissipated = {1,2,3};
-//     arma::Mat<double> Wstar = {1,2,3};
-//     double time = 0;
-//     std::vector<double> q = {1,2,3};
-//     EXPECT_EQ(c0->conservate(input,inputDissipated,Wstar,time,q)(0),0.5);
-// }
+TEST_F(ConservationModelTesting, conservateWorksZeroEdges) {
+    double time = 0;
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time,q)(0),1);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time,q)(1),2);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time,q)(2),3);
+}
+
+TEST_F(ConservationModelTesting, conservateWorksZeroScaleFunctionZeroEdges) {
+    double time = 0;
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time)(0),1.0);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time)(1),2.0);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_zeros,time)(2),3.0);
+    EXPECT_DOUBLE_EQ(c1->conservate(input,inputDissipated,Wstar_zeros,time,q)(0),1);
+    EXPECT_DOUBLE_EQ(c1->conservate(input,inputDissipated,Wstar_zeros,time,q)(1),2);
+    EXPECT_DOUBLE_EQ(c1->conservate(input,inputDissipated,Wstar_zeros,time,q)(2),3);
+}
+
+TEST_F(ConservationModelTesting, conservateWorksOneEdge) {
+    double time = 0;
+
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time)(0),0.5);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time)(1),2.0);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time)(2),3.0);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time,q)(0),0.5);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time,q)(1),2.0);
+    EXPECT_DOUBLE_EQ(c0->conservate(input,inputDissipated,Wstar_oneEdge,time,q)(2),3.0);
+}
 
