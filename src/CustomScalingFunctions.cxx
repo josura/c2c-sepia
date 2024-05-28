b9360dde282e97902d7c92bdc58d069f40c0b7ce
josura <giorgiolocicero@hotmail.com>
2024-05-28 12:29:31 +0200

    changed default custom scaling functions

--- src/CustomScalingFunctions.cxx
+++ src/CustomScalingFunctions.cxx
@@ -1,24 +1,26 @@
 #include "CustomScalingFunctions.h"
 
-//add scaling function as lambda function for dissipation
+
+//add scaling function as lambda function for dissipation, value should be between 0 and 1 to not get negative values
 std::function<double(double)> getDissipationScalingFunction(){
     return [](double x) {
         //rewrite accordingly
-        return 1.0;      
+        return 0.5;
+        
     };
 }
 
-//add scaling function as lambda function for conservation
+//add scaling function as lambda function for conservation, should also be between 0 and 1 to not get negative values
 std::function<double(double)> getConservationScalingFunction(){
     return [](double x) {
         //rewrite accordingly
-        return 1.0;      
+        return 0.5;      
     };
 }
 
 std::function<double(double)> getPropagationScalingFunction(){
     return [](double x) {
         //rewrite accordingly
-        return 1.0;      
+        return 0.5;
     };
 }
