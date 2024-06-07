4e4860bcadf7b6fc97ff9a1678299579f02ae191
josura <giorgiolocicero@hotmail.com>
2024-06-07 12:33:58 +0200

    testing for new augmented computation since it was not present

--- src/ComputationTesting.cc
+++ src/ComputationTesting.cc
@@ -5,6 +5,12 @@
 #include "Computation.h"
 #include "Matrix.h"
 #include "utilities.h"
+#include "DissipationModel.h"
+#include "DissipationModelScaled.h"
+#include "ConservationModel.h"
+#include "PropagationModel.hxx"
+#include "PropagationModelNeighbors.hxx"
+#include "PropagationModelOriginal.hxx"
 
 class ComputationTesting : public ::testing::Test {
     protected:
@@ -16,6 +22,10 @@ class ComputationTesting : public ::testing::Test {
         void TearDown() override{
             //delete c0;
             delete c1;
+            delete dissipationModelNull;
+            delete dissipationModelHalf;
+            delete conservationModelNull;
+            delete conservationModelHalf;
         }
 
 
@@ -45,7 +55,13 @@ class ComputationTesting : public ::testing::Test {
 
         Computation* c0;       //testing default constructor
         Computation* c1;       //testing general constructor
-  
+
+        DissipationModel* dissipationModelNull = new DissipationModelScaled([](double time){return 0.0;});
+        DissipationModel* dissipationModelHalf = new DissipationModelScaled([](double time){return 0.5;});
+
+        ConservationModel* conservationModelNull = new ConservationModel([](double time){return 0.0;});
+        ConservationModel* conservationModelHalf = new ConservationModel([](double time){return 0.5;});
+
 };
 
 TEST_F(ComputationTesting, constructorWorksDefault) {
@@ -482,6 +498,50 @@ TEST_F(ComputationTesting, testAugmentedVinputZeros){
     EXPECT_NEAR(perturbation[augMeta->getIndexFromName("v-in:testCell3")], 0,1e-12);
     ASSERT_TRUE(augMeta->getIndexFromName("v-in:testCell2") >= 0 && augMeta->getIndexFromName("v-in:testCell4") < 12);
     EXPECT_NEAR(perturbation[augMeta->getIndexFromName("v-in:testCell4")], 0,1e-12);
+}
+
+TEST_F(ComputationTesting,testNewAugmentedVinputZeros){
+    Computation computationTest;
+    computationTest.assign(*c1);
+    computationTest.augmentGraph(cellTypes);
+    computationTest.addEdges(virtualInputEdges,virtualInputEdgesValues);
+    computationTest.addEdges(virtualOutputEdges,virtualOutputEdgesValues);
+    // set propagation model
+    PropagationModel* propagationModel = new PropagationModelOriginal(computationTest.getAugmentedGraph());
+    computationTest.setConservationModel(conservationModelNull);
+    computationTest.setDissipationModel(dissipationModelNull);
+    computationTest.setPropagationModel(propagationModel);
+    auto perturbation = computationTest.computeAugmentedPerturbationEnhanced4(0);
+    computationTest.updateInput(perturbation,true);
+    auto computationInputAfter = computationTest.getInputAugmented();
+    EXPECT_EQ(computationInputAfter.size(), perturbation.size());
+    for (uint i = 0; i< computationInputAfter.size(); i++) {
+        EXPECT_DOUBLE_EQ(perturbation[i], computationInputAfter[i]);
+    }
+    EXPECT_EQ( perturbation.size(), 12);
+    EXPECT_EQ(computationTest.getInput().size(),6);
+    EXPECT_EQ(computationTest.getOutput().size(),0);
+    EXPECT_EQ(computationTest.getInputAugmented().size(),12);
+    EXPECT_EQ(computationTest.getOutputAugmented().size(),12);
+    EXPECT_EQ(computationTest.getLocalCellType(),"testCell");
+    auto meta = computationTest.getGraph();
+    auto augMeta = computationTest.getAugmentedGraph();
+    ASSERT_TRUE(meta != nullptr);
+    EXPECT_EQ(meta->getNumNodes(),6);
+    EXPECT_EQ(meta->getNumEdges(),30);
+    ASSERT_TRUE(augMeta != nullptr);
+    EXPECT_EQ(augMeta->getNumNodes(),12);
+    EXPECT_EQ(augMeta->getNumEdges(),36);
+    EXPECT_EQ(computationTest.getCellTypes().size(),3);
+    ASSERT_TRUE(augMeta->getIndexFromName("v-in:testCell") < 0);
+    ASSERT_TRUE(augMeta->getIndexFromName("v-in:testCell2") >= 0 && augMeta->getIndexFromName("v-in:testCell2") < 12);
+    EXPECT_NEAR(perturbation[augMeta->getIndexFromName("v-in:testCell2")], 0, 1e-12);
+    ASSERT_TRUE(augMeta->getIndexFromName("v-in:testCell2") >= 0 && augMeta->getIndexFromName("v-in:testCell3") < 12);
+    EXPECT_NEAR(perturbation[augMeta->getIndexFromName("v-in:testCell3")], 0,1e-12);
+    ASSERT_TRUE(augMeta->getIndexFromName("v-in:testCell2") >= 0 && augMeta->getIndexFromName("v-in:testCell4") < 12);
+    EXPECT_NEAR(perturbation[augMeta->getIndexFromName("v-in:testCell4")], 0,1e-12);
+    delete propagationModel;
+
 }
 
 //TESTING IF NODE VALUES FOR v-input nodes are the same as the previous iteration
