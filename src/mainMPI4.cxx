249f461b5b025cba21b4bb13aca44af15ded57ec
josura <giorgiolocicero@hotmail.com>
2024-07-04 13:14:48 +0200

    removed single graph nodes parameter since it was creating so many problems

--- src/mainMPI4.cxx
+++ src/mainMPI4.cxx
@@ -51,7 +51,6 @@ int main(int argc, char** argv) {
         ("initialPerturbationPerTypeFolder", po::value<std::string>(), "(string) initialPerturbationPerType folder, for an example see in data data/testdata/testGraph/initialValues")
         ("typeInteractionFolder", po::value<std::string>(), "(string) directory for the type interactions, for an example see in data data/testdata/testHeterogeneousGraph/interactions")
         ("nodeDescriptionFile", po::value<std::string>(), "(string) node description file, used to generate the output description in the case of common graph between types, if not specified no names are used. For an example see in data resources/graphs/metapathwayNew/nodes.tsv")
-        ("homogenousGraphNodesFile", po::value<std::string>(), "(string) nodes folder, where the files containing the description/nodes for all the graphs are contained, used to read the graph nodes, if not specified the graphs will be built with the edges files(could not contain some isolated nodes) for an example see one of the graphs in data/testdata/testHeterogeneousTemporalGraph/nodesDescriptionDifferentStructure")
         ("nodeDescriptionFolder", po::value<std::string>(), "(string) nodes folder, where the files containing the description/nodes for all the graphs are contained, used to read the graph nodes, if not specified the graphs will be built with the edges files(could not contain some isolated nodes) for an example see the folder structure in data/testdata/testHeterogeneousTemporalGraph/nodesDescriptionDifferentStructure")
         ("ensembleGeneNames",po::bool_switch(&ensembleGeneNames),"() use ensemble gene names, since the graph used in resources have entrez_ids, a map will be done from ensemble to entrez, the map is available in resources")
         ("sameTypeCommunication",po::bool_switch(&sameTypeCommunication),"() use same type communication, since it is not permitted as the standard definition of the model, this adds a virtual node for the same type type")
@@ -483,9 +482,6 @@ int main(int argc, char** argv) {
         std::cerr << "[ERROR] homogenousGraphNodesFile and nodeDescriptionFolder were both set, only one can be used when using an homogeneous configuration for the agent or with different structured agents. Aborting\n";
         return 1;
     }
-
-    std::string homogenousGraphNodesFile="";
-
     std::string nodesDescriptionFolder="";
     if(vm.count("nodeDescriptionFolder")){
         logger <<"[LOG] using node description folder to get the names of the nodes in the graphs"<<std::endl;
