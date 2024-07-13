162c1a0a581766d681e98be0558b5b79fcd95289
josura <giorgiolocicero@hotmail.com>
2024-07-13 15:38:53 +0200

    plotting for fixed features in a grid

--- scripts/R/singleCell/showTimeDataForBatches.R
+++ scripts/R/singleCell/showTimeDataForBatches.R
@@ -5,6 +5,8 @@ timeSeriesDirectory <- "/home/josura/Projects/ccc/c2c-sepia/outputsTimeSeries/da
 
 readTimeSeries <- function(timeSeriesDirectory) {
   files <- list.files(timeSeriesDirectory, full.names = TRUE)
+  #remove the fullgraph_output.tsv file
+  files <- files[!grepl("fullgraph_output.tsv", files)]
   timeSeries <- files  %>%
     lapply(read_tsv)
   # remove .x from the column names, when present
@@ -38,3 +40,20 @@ timeSeries_all <- timeSeries[[1]]
 for (i in 2:length(timeSeries)) {
   timeSeries_all <- rbind(timeSeries_all, timeSeries[[i]])
 }
+
+features_to_visualize <- c("Kras","Trp53","Egfr","Rb1","Nf1","Myc","Braf")
+
+install.packages("gridExtra")
+library(gridExtra)
+
+plotList <- list()
+for (feature in features_to_visualize) {
+  p <- ggplot(timeSeries_all, aes_string(x = "time", y = feature, color = "type")) +
+    geom_point() +
+    geom_line() +
+    labs(title = feature) +
+    theme_minimal()
+  plotList[[feature]] <- p
+}   
+
+do.call(grid.arrange, c(plotList, ncol = 3))
