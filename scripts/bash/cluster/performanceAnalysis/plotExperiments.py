7f01bd696de5a1fe0ca82f514e367967fe19fd47
josura <giorgiolocicero@hotmail.com>
2024-08-29 09:17:35 +0200

    added speedup to experiments, to correct since it is not the correct way of computing speedup

--- scripts/bash/cluster/performanceAnalysis/plotExperiments.py
+++ scripts/bash/cluster/performanceAnalysis/plotExperiments.py
@@ -10,10 +10,10 @@ import os
 # graphNodesNumber	numberProcesses	numberTypes	numberIterations	time
 
 # read the data
-data = pd.read_csv("/home/josura/Projects/ccc/c2c-sepia/scripts/bash/cluster/performanceAnalysis/performanceTimes.tsv", sep="\t")
+data = pd.read_csv("/home/josura/Projects/ccc/c2c-sepia/scripts/bash/cluster/performanceAnalysis/localPerformanceTimes.tsv", sep="\t")
 
-# add a column with the average time, group by the number of nodes and the number of processes
-data["averageTime"] = data.groupby(["graphNodesNumber", "numberProcesses"])["time"].transform("mean")
+# create a new dataframe with the average time, group by the number of nodes and the number of processes
+data = data.groupby(["graphNodesNumber", "numberProcesses"]).agg({"time": "mean"}).reset_index()
 
 # plot in 3d the number of processes, the number of nodes and the time
 fig = plt.figure()
@@ -22,4 +22,17 @@ ax.scatter(data["graphNodesNumber"], data["numberProcesses"], data["averageTime"
 ax.set_xlabel('Number of Nodes')
 ax.set_ylabel('Number of Processes')
 ax.set_zlabel('Time')
+plt.show()
+
+# add column with the speedup per number of nodes and number of processes
+# speedup = avg_time(1 processor) / avg_time(n processors)
+data["speedup"] = data.groupby(["graphNodesNumber", "numberProcesses"])["averageTime"].transform("min") / data["averageTime"]
+
+# plot the speedup
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(data["graphNodesNumber"], data["numberProcesses"], data["speedup"])
+ax.set_xlabel('Number of Nodes')
+ax.set_ylabel('Number of Processes')
+ax.set_zlabel('Speedup')
 plt.show()
