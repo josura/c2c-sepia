a3fd1e09a5fd98fa7be24e7d159743706e562ed3
josura <giorgiolocicero@hotmail.com>
2024-11-22 15:03:20 +0100

    showing plot of the cells in liana testing

--- scripts/python/temporalSingleCell/lianaTesting.py
+++ scripts/python/temporalSingleCell/lianaTesting.py
@@ -4,14 +4,13 @@ import mudata as mu
 import scanpy as sc
 
 from matplotlib import pyplot as plt
-from adjustText import adjust_text
-
-
-kwargs = {'frameon':False, 'size':1.5, 'img_key':'lowres'}
+from plotly import express as px
 
 # Load the data
-prot = sc.read('temporalSingleCell/citeseq_prot.h5ad', backup_url='https://figshare.com/ndownloader/files/47625196')
-rna = sc.read('temporalSingleCell/citeseq_rna.h5ad', backup_url='https://figshare.com/ndownloader/files/47625193')
+# prot = sc.read('temporalSingleCell/citeseq_prot.h5ad', backup_url='https://figshare.com/ndownloader/files/47625196')
+# rna = sc.read('temporalSingleCell/citeseq_rna.h5ad', backup_url='https://figshare.com/ndownloader/files/47625193')
+prot = sc.read('temporalSingleCell/citeseq_prot.h5ad')
+rna = sc.read('temporalSingleCell/citeseq_rna.h5ad')
 
 mdata = mu.MuData({'rna': rna, 'prot': prot})
 # make sure that cell type is accessible
@@ -20,5 +19,9 @@ mdata.obs['celltype'] = mdata.mod['rna'].obs['celltype'].astype('category')
 mdata
 
 # show the data
-fig = plt.figure(figsize=(10, 10))
-sc.pl.embedding(rna, basis='umap', color='celltype', ax=fig.gca(), **kwargs)
+X = list(map(lambda x: x[0], rna.obsm["X_umap"]))
+Y = list(map(lambda x: x[1], rna.obsm["X_umap"]))
+celltypes = list(rna.obs["celltype"].values)
+
+fig = px.scatter(x=X, y=Y, color=celltypes)
+fig.show()
