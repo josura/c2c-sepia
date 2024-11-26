410098032a9e09a96621f63de6a3a5176666d00d
josura <giorgiolocicero@hotmail.com>
2024-11-26 12:43:05 +0000

    returned to original example moved liana testing for mouse data elsewhere

--- scripts/python/temporalSingleCell/lianaTesting.py
+++ scripts/python/temporalSingleCell/lianaTesting.py
@@ -77,13 +77,6 @@ plot.show()
 ## Focus on Transcriptomics Data
 adata = mdata.mod['rna']
 
-
-## convert to murine symbols from human to mouse, vignette available at: https://liana-py.readthedocs.io/en/latest/notebooks/sma.html
-map_df = li.rs.get_hcop_orthologs(columns=['human_symbol', 'mouse_symbol'],
-                                  min_evidence=3
-                                  ).rename(columns={'human_symbol':'source',
-                                                   'mouse_symbol':'target'})
-
 ## Obtain MetalinksDB Prior Knowledge
 metalinks = li.resource.get_metalinks(biospecimen_location='Blood',
                                       source=['CellPhoneDB', 'Cellinker', 'scConnect', # Ligand-Receptor resources
@@ -92,29 +85,21 @@ metalinks = li.resource.get_metalinks(biospecimen_location='Blood',
                                       types=['pd', 'lr'], # NOTE: we obtain both ligand-receptor and production-degradation sets
                                      )
 
-metalinks_translated = li.rs.translate_column(resource=metalinks,
-                                   map_df=map_df,
-                                   column='gene_symbol',
-                                   one_to_many=1)
-metalinks_translated.head()
-
-
-
 ## preparing the metabolite-receptor pairs
-resource = metalinks_translated[metalinks_translated['type']=='lr'].copy()
+resource = metalinks[metalinks['type']=='lr'].copy()
 resource = resource[['metabolite', 'gene_symbol']]\
     .rename(columns={'gene_symbol':'receptor'}).drop_duplicates()
 resource.head()
 
 ## Prepare the Production-Degradation Network
-pd_net = metalinks_translated[metalinks_translated['type'] == 'pd']
+pd_net = metalinks[metalinks['type'] == 'pd']
 # we need to aggregate the production-degradation values
 pd_net = pd_net[['metabolite', 'gene_symbol', 'mor']].groupby(['metabolite', 'gene_symbol']).agg('mean').reset_index()
 pd_net.head()
 
 
 ## Prepare the Transport Network
-t_net = metalinks_translated[metalinks_translated['type'] == 'pd']
+t_net = metalinks[metalinks['type'] == 'pd']
 t_net = t_net[['metabolite', 'gene_symbol', 'transport_direction']].dropna()
 # Note that we treat export as positive and import as negative
 t_net['mor'] = t_net['transport_direction'].apply(lambda x: 1 if x == 'out' else -1 if x == 'in' else None)
@@ -131,4 +116,7 @@ meta = li.mt.fun.estimate_metalinks(adata,
                                     weight='mor', min_n=3)
 # pass cell type information
 meta.obs['celltype'] = adata.obs['celltype']
+
+with plt.rc_context({"figure.figsize": (5, 5), "figure.dpi": (100)}):
+    sc.pl.umap(meta.mod['metabolite'], color=['Prostaglandin J2', 'Metanephrine', 'celltype'], cmap='coolwarm')
 
