2123e8e55e0a0a37496696d1c3459e0d9a9a3fa9
josura <giorgiolocicero@hotmail.com>
2025-03-19 13:18:29 +0000

    add: reading the files where the outputs for single cell are stored into the datastructure

--- scripts/python/temporalSingleCell/plotResults_withLR.py
+++ scripts/python/temporalSingleCell/plotResults_withLR.py
@@ -5,3 +5,9 @@ import os
 # get the outputs(iteration matrices) from the folder
 iterationMatrixFolder = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/outputWithLR/iterationMatrices"
 iterationMatrices = {}
+
+
+# get the files in the folder, every single file is a type(the name is specified by the first part of the filename), and the content is the iteration matrix
+for file in os.listdir(iterationMatrixFolder):
+    if file.endswith(".tsv"):
+        iterationMatrices[file.split('_')[0]] = pd.read_csv(iterationMatrixFolder + '/' + file, sep='\t')
