e584899ece24ed08d88be715a7269f17838a99fa
josura <giorgiolocicero@hotmail.com>
2025-03-19 13:17:49 +0000

    add: definition of the file where the iteration matrices are saved and the data structure where they are loaded (a dictionary)

--- scripts/python/temporalSingleCell/plotResults_withLR.py
+++ scripts/python/temporalSingleCell/plotResults_withLR.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+
+# get the outputs(iteration matrices) from the folder
+iterationMatrixFolder = "/home/josura/Projects/ccc/datiIdo/inputGraphs/1h/outputWithLR/iterationMatrices"
+iterationMatrices = {}
