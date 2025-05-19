1708ae966aa38e92b346691c92913bf9e68c000b
josura <giorgiolocicero@hotmail.com>
2025-05-19 13:42:01 +0000

    feature: continuing with the implementation of the computation of the MSE, using common names

--- scripts/python/temporalSingleCell/compute-MSE-metabolites.py
+++ scripts/python/temporalSingleCell/compute-MSE-metabolites.py
@@ -112,17 +112,22 @@ else:
 ### reading the name_map
 namemap = pd.read_csv("/home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/name_map_full_mouse_fixed.csv", sep=',')
 source_namemap = pd.read_csv("/home/josura/Projects/ccc/c2c-sepia/scripts/python/temporalSingleCell/KEGG-to-original-names-all.tsv", sep='\t')
-### change the column names in the metabolites dataframes to the real original names (the ids are the column called Query, and the original names are in Match)
+### change the column names in the metabolites dataframes to the HMDB names, there needs to be a translation from (source names) -> KEGG ids -> HMDB
 node_name_list = []
 for i in range(len(metabolites_1h_averaged.columns)):
     metaboliteToChange = metabolites_1h_averaged.columns[i]
-    real_name_pd = namemap[namemap["Query"] == metaboliteToChange]["Match"]
-    real_name = metaboliteToChange
-    if len(real_name_pd) > 0:
-        real_name = real_name_pd.values[0]
+    kegg_name_pd = source_namemap[source_namemap["name"] == metaboliteToChange]["KEGG"]
+    #real_name_pd = namemap[namemap["Query"] == metaboliteToChange]["Match"]
+    final_name = metaboliteToChange
+    if len(kegg_name_pd) > 0:
+        HMDB_name_pd = namemap[namemap["KEGG"] == kegg_name_pd.values[0]]["HMDB"].dropna()
+        if(len(HMDB_name_pd) > 0):
+            final_name = HMDB_name_pd.values[0]
+        else:
+            print("The metabolite " + metaboliteToChange + " doesn't have a HMDB identifier")
     else:
         print("The metabolite " + metaboliteToChange + " doesn't have a real name")
-    node_name_list.append(real_name)
+    node_name_list.append(final_name)
 
 metabolites_1h_averaged.columns = node_name_list
 metabolites_6h_averaged.columns = node_name_list
