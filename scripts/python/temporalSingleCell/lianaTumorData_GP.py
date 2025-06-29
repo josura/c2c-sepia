785bd270a2cd06c3726c39ec7ebb0ec30814f6af
josura <giorgiolocicero@hotmail.com>
2025-06-30 00:16:20 +0200

    feature: visualization of the IPF estimated metabolitess

--- scripts/python/temporalSingleCell/lianaTumorData_GP.py
+++ scripts/python/temporalSingleCell/lianaTumorData_GP.py
@@ -133,6 +133,11 @@ meta_IPF = li.mt.fun.estimate_metalinks(rna=rna_IPF,
 # pass cell type information
 meta_IPF.obs['celltype'] = adata.obs['celltype']
 
+#dataset with two modalities, one for RNA and one for Metabolites. The metabolites are estimated as t-values.
+# visualization 
+with plt.rc_context({"figure.figsize": (5, 5), "figure.dpi": (100)}):
+    sc.pl.umap(meta.mod['metabolite'], color=['Prostaglandin J2', 'Metanephrine', 'celltype'], cmap='coolwarm')
+
 
 
 ## Normal
